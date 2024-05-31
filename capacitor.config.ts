644d021b03cdf3ac6a9c9a5f078d43a2327823c1
root <root@localhost.localdomain>
2024-05-31 17:40:31 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'INVERNADERO-CONTROL-FRONTEND',
+  webDir: 'www'
+};
+
+export default config;
