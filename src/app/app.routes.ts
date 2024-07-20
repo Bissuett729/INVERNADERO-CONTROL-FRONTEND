0b0b3ee4dfc2128b79dfb37fd8fa34c5eee65cd0
Miguel Angel Bissuett Lira <miguelbissuett@fii-na.com.mx>
2024-07-20 05:34:16 -0600

    Actualizacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'auth', pathMatch: 'full' },
-  { path: 'auth', loadChildren: () => import('./auth/auth.routes').then(m=>m.authRoutes) }
+  { path: '', redirectTo: 'greenhouse/auth', pathMatch: 'full' },
+  { path: 'greenhouse/auth', loadChildren: () => import('./auth/auth.routes').then(m=>m.authRoutes) },
 
 
 
-  // { path: 'invernadero', loadComponent: () => import('./layout/layout.component').then(m => m.LayoutComponent), children: [
-  //   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  //   { path: 'home', loadComponent: () => import('./layout/pages/home/home.component').then(m=>m.HomeComponent) }
-  // ] },
+  { path: 'greenhouse', loadComponent: () => import('./layout/layout.component').then(m => m.LayoutComponent), children: [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', loadComponent: () => import('./layout/pages/home/home.component').then(m=>m.HomeComponent) }
+  ] },
 
 ];
