2d3470253bd40c76f6f8693344962a9d3734f538
Miguel Angel Bissuett Lira <miguelbissuett@fii-na.com.mx>
2024-07-20 05:03:37 -0600

    Actualizacion

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -5,7 +5,8 @@ export const authRoutes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', loadComponent: () => import('./login/login.page').then(m=>m.LoginPage) },
     { path: 'register', loadComponent: () => import('./register/register.page').then(m=>m.RegisterPage) },
-    { path: 'forgot-password', loadComponent: () => import('./forgot-password/forgot-password.page').then(m=>m.ForgotPasswordPage) }
+    { path: 'forgot-password', loadComponent: () => import('./forgot-password/forgot-password.page').then(m=>m.ForgotPasswordPage) },
+    { path: 'change-password', loadComponent: () => import('./change-password/change-password.page').then(m=>m.ChangePasswordPage) }
 
   ]},
 ];
